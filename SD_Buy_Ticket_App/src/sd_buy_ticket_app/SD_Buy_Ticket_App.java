dbe1f0ec9ac072770e84bd25dac7f982286b3193
Haq11 <31028072+Haq11@users.noreply.github.com>
2018-07-11 12:18:07 +0530

    Initial Commit

---
+++ SD_Buy_Ticket_App/src/sd_buy_ticket_app/SD_Buy_Ticket_App.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd_buy_ticket_app;
+
+/**
+ *
+ * @author HaqHfz
+ */
+public class SD_Buy_Ticket_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
